a8da1ab2fbe2a7222566a9344536630c26ff27ae
egyu1 <egyu1333@gmail.com>
2025-11-12 03:17:35 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void) {
+	
+	int paper[100][100] = { 0 };
+	int n;
+	int count = 0;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+
+		for (int j = y; j < y + 10; j++) {
+			for (int k = x; k < x + 10; k++) {
+				paper[j][k] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++) {
+			if (paper[i][j] == 1) {
+				count++;
+			}
+		}
+	}
+
+	printf("%d\n", count);
+
+	return 0;
+}
